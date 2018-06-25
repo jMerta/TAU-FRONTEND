d2303dea5b0ba0b729bf840ed9bfe765b7a041f7
Jakub Merta <jakubm@lsnova.pl>
2018-06-25 10:47:25 +0200

    protractor

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,30 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to TAU-FRONTEND!');
+    expect(page.getParagraphText()).toEqual('Almost Jira');
   });
+
+  it('should create navigation bar', () => {
+    page.navigateTo();
+    expect(page.getNavbarElement()).toBeTruthy();
+  });
+
+  it('should log in user', () => {
+    browser.driver.get('http://localhost:4200/register');
+    const user = browser.driver.findElement(by.name('username'));
+    const password = browser.driver.findElement(by.name('password'));
+
+    const button = element(by.className('submit'));
+
+    user.sendKeys('first');
+    password.sendKeys('nope');
+
+    expect(user.getAttribute('value')).toEqual('first');
+    expect(password.getAttribute('value')).toEqual('nope');
+
+    button.click().then(() => {
+      browser.waitForAngular();
+      expect(browser.driver.get('http://localhost:4200/dashboard'));
+    });
+  };
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -8,4 +8,8 @@ export class AppPage {
   getParagraphText() {
     return element(by.css('app-root h1')).getText();
   }
+
+  getNavbarElement() {
+    return element(by.css('app-root app-header header'));
+  }
 }
